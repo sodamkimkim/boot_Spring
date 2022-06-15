62f499755329990669c038294c5f3593358041ae
sodamkimkim <100888879+sodamkimkim@users.noreply.github.com>
2022-06-15 14:07:28 +0900

    mvc모델

---
+++ mvcdemo-1/src/main/java/com/example/mvcdemo1/Mvcdemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.mvcdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvcdemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mvcdemo1Application.class, args);
+	}
+
+}
---
+++ mvcdemo-1/src/test/java/com/example/mvcdemo1/Mvcdemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvcdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvcdemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
